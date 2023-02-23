bdccbcfe801f1b8f1003121f7f9d48107362de03
Sabrina Alves Brito <sabrina.a.brito@gmail.com>
2023-02-23 14:44:41 -0300

    fix jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,30 +7,27 @@ export function Home() {
   const [characterCardName, setCharacterCardName] = useState("");
   let [characterCard, setCharacterCard] = useState({});
 
-  async function serchForCharacter() {
+  async function searchForCharacter() {
     if(characterCardName === ''){
-      alert("Preencha algum nome!")
+      alert("Preencha algum nome!");
       return;
     }
     try{
-      const response = await fetch('https://hp-api.herokuapp.com/api/characters')
-      const data = await response.json()
+      const response = await fetch('https://hp-api.onrender.com/api/characters');
+      const data = await response.json();
       for (let i in data) {
-        /* if(characterCardName != data[i].name){
-          alert("Nome escrito errado!")
-          return;
-        } */
-        if(data[i].name == characterCardName){
+        if(data[i].name.toLowerCase().includes(characterCardName) || 
+        data[i].house.toLowerCase().includes((characterCardName))) {
           setCharacterCard(data[i]);
         }
       }
     }
     catch{
-      alert("Erro ao buscar.")
-      setCharacterCardName(""); 
+      alert("Erro ao buscar.");
+      setCharacterCardName("");
     } 
   }
-
+  
   return (
     <div className="container">
       <header>
@@ -42,10 +39,11 @@ export function Home() {
         <section className='inputButton'>
           <input
             type="text"
+            id='input'
             placeholder="Type here..."
             onChange={e => setCharacterCardName(e.target.value)}
            />
-           <button type='button' onClick={serchForCharacter}>
+           <button type='button' onClick={searchForCharacter} id='button'>
             <FiSearch size={25} color="#000"/>
            </button>
         </section>
@@ -60,6 +58,3 @@ export function Home() {
     </div>
   )
 }
-
-
-{/* {Object.keys(cep).length > 0 && ()} */}
